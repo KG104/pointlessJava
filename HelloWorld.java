8e6903df431ca1a247031cd6638dee4e49b8ca53
KG104 <konstantin.gerlach@kabelmail.de>
2024-07-21 20:15:57 +0200

    adding .gitignore

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+// hof
+
+import java.lang.String;
+
+public class HelloWorld extends Object {
+	public static void main (String args[]) {
+		if(1==1) {
+      for(int i=0; i<1; i++) {
+        System.out.println("Hello World");
+      }
+    }
+	}
+}
+
+// eof
